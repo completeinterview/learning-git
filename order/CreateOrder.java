a9beb76a4eace51d1c2989d6124a8df443129a0c
Aakash Verma <info.aakash11@gmail.com>
2024-12-10 18:52:56 +0530

    added create order logic

---
+++ order/CreateOrder.java
@@ -0,0 +1,10 @@
+class CreateOrder {
+
+    public static void createOrder() {
+        System.out.println("Order is created successfully.");
+    }
+
+    public static void main(String[] args) {
+        createOrder();
+    }
+}
