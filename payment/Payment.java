d52c0104dc03f4234b2dac7895d6bbedfe528e8f
Aakash Verma <info.aakash11@gmail.com>
2024-12-11 18:02:05 +0530

    payment logic is added

---
+++ payment/Payment.java
@@ -0,0 +1,11 @@
+package payment;
+
+public class Payment {
+
+    public static void makePayment() {
+        System.out.println("Payment is made successfully");
+    }
+    public static void main(String[] args) {
+        makePayment();
+    }
+}
